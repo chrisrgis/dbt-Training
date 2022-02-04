6ec476aa4a134670f81ab0d74b1ce433c5579b69
Chris Robinson <chrisr.gis@gmail.com>
2022-02-04 00:39:05 +0000

    Commit v3

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,12 @@
-select
+with customers as (
+    
+    select
     id as customer_id,
     first_name,
     last_name
 
 from dbt_crobinson.customers
+
+)
+
+select * from customers
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,16 @@
-select
+with payment as (
+    
+    select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
 
-from dbt_crobinson.payment 
+from dbt_crobinson.payment
+
+)
+
+select * from payment
